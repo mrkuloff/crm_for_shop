f603def25b5a21c206ca632fc2c56fc34df0ba7f
unknown <ae-m7@mail.ru>
2022-04-20 22:40:37 +0300

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+let nameGood; //Наименование товара
+let amountGood; //Количество товара
+let categoryGood; //Категория товара
+let priceGood; //Цена товара
+
+
+console.log(nameGood);
+
+console.log(priceGood * amountGood);
