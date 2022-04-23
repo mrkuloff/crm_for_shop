d02d6a39cbec93c0bfc3a00dd5eaf44be851b792
unknown <ae-m7@mail.ru>
2022-04-23 19:52:05 +0300

    task upd

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const nameGood = prompt('Наименование товара', "defaultName");
-const amountGood = parseInt(prompt('Количество товара' , '0'), 10);
+const amountGood = parseInt(prompt('Количество товара' , 0), 10);
 const categoryGood = prompt('Категория товара' , 'defaultCategory');
-const priceGood = parseInt(prompt('Цена товара' , '0'), 10);
+const priceGood = parseInt(prompt('Цена товара' , 0), 10);
 
 if (Number.isInteger(amountGood) && Number.isInteger(priceGood)) {
   console.log("Введено число");
--- js/tasks.js
+++ js/tasks.js
@@ -7,3 +7,24 @@ if (rain===1) {
 } else if (rain===0) {
   console.log(`Дождя нет`)
 }
+
+//АРТУР, нужно набрать 265 баллов
+const math = parseInt(prompt('Математика', 0), 10);
+const russian = parseInt(prompt('Русский' , 0), 10);
+const informatik = parseInt(prompt('Информатика' , 0), 10);
+
+if (math + russian + informatik >= 265) {
+  console.log(`Вы на бюджете! :D`);
+} else {
+  console.log(`Вы не набрали нужного количества баллов. Ваш балл ${math + russian + informatik}`);
+}
+
+
+//человек и банкомат
+const money = prompt('Сколько вы хотите снять дерег?', "0");
+
+if (money / 100 | 0) {
+  console.log(`Запускается процесс снятия купюр`);
+} else {
+  console.log(`Мы не можем выдать сумму, введите другое число`);
+}
